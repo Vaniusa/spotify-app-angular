2f4ec6a2c367bfd381760557fbebb52b9f6baddd
UbuntuPc <ionsaitan1980@gmail.com>
2019-05-27 07:27:43 +0200

    Buscar artista

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,9 +12,17 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQBnqDYfS2w14zHZ7052c7rDoDWcBi8cnmD_HqVlFn6s5Cf_88jsGiwcyllrOdSDVQdiTvLrLVjh5Ca5e1U'
+      Authorization: 'Bearer BQCieBg4SrgytgH9B3G0rLHBEJ3X-j7spykuj2Syu6NZD7NC27W8dXwIogqlfmEMnUUXvJh6tdWexSQIVEg'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQCieBg4SrgytgH9B3G0rLHBEJ3X-j7spykuj2Syu6NZD7NC27W8dXwIogqlfmEMnUUXvJh6tdWexSQIVEg'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {headers});
   }
 }
